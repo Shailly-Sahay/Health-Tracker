9e28b244bde2a11bc6bca893dfc3aa85003cc6f8
Shailly-Sahay <shaillysahayy@gmail.com>
2025-01-30 21:22:47 +0530

    fix(routing): Ensure SPA behavior by configuring Angular standalone routing

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,10 +1,12 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconField } from 'primeng/iconfield';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
+  standalone: true,
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -12,9 +14,9 @@ import { IconField } from 'primeng/iconfield';
 export class LayoutComponent {
   isCollapsed = false;
   items: any[] = [
-    { label: 'Home', icon: 'home-outline' },
-    { label: 'Profile', icon: 'person-circle-outline' },
-    { label: 'Settings', icon: 'settings-outline' },
+    { label: 'Home', href: '/', icon: 'home-outline' },
+    { label: 'Workouts', href: '/workouts', icon: 'person-circle-outline' },
+    { label: 'Progress', href: '/workout-progress', icon: 'settings-outline' },
   ];
 
   toggleMenu() {
